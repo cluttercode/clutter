abfb10112d74d2db5c12222c8e8699aef4b9774c
Itay Donanhirsh <itay.donanhirsh@checkr.com>
2021-01-29 23:38:22 -0800

    add instruction to run make clutter before test e2e

--- tests/cli/run.sh
+++ tests/cli/run.sh
@@ -11,6 +11,11 @@ TESTS="${1-*.test}"
 
 export CLUTTER="../../bin/clutter"
 
+if [[ ! -x ${CLUTTER} ]]; then
+  echo "error: clutter need to be built first. run: make clutter".
+  exit 1
+fi
+
 for t in ${TESTS}; do
   printf "${BWHITE}${t}${NC}:\n"
   ./clitest "$t"
